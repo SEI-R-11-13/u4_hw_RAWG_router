bb27faeabb994353eac3ff8bd371474de9514561
HarmonyMorse <harmonythedev@gmail.com>
2024-01-31 13:13:13 -0800

    Refactor GenreCard with props

--- src/components/GenreCard.jsx
+++ src/components/GenreCard.jsx
@@ -1,12 +1,13 @@
-const GenreCard = () => {
+const GenreCard = ({ image, name, gamesCount, onClick }) => {
 
   return (
-    <div className="card">
+    <div className="card" onClick={onClick}>
       <div className="img-wrapper">
-
+        <img src={image} alt={`${name} genre`} />
       </div>
       <div className="info-wrapper flex-col">
-        
+        <h3>{name}</h3>
+        <p>{gamesCount}</p>
       </div>
     </div>
   )
