45db31ef36c30e70b724f04c993f338a68f6637f
HarmonyMorse <harmonythedev@gmail.com>
2024-01-31 12:53:01 -0800

    Add routes skeleton to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './styles/App.css'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 
 const App = () => {
 
   return (
     <div>
-
+      <header><Header /></header>
+      <main>
+        <Routes>
+          {/* <Route path="" element={} /> */}
+          {/* <Route path="" element={} /> */}
+          {/* <Route path="" element={} /> */}
+          {/* <Route path="" element={} /> */}
+        </Routes>
+      </main>
     </div>
   )
 }
