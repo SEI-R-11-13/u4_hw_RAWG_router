1df2d8f5508e498cb1f82b9a465b5f75d93ce3b2
Rachel Carleton <rachel.carleton@icloud.com>
2024-01-31 08:44:05 -0800

    finished homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './styles/App.css'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header' 
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import ViewGames from './pages/ViewGames.jsx'
+import GameDetails from './pages/GameDetails.jsx'
 
 const App = () => {
 
   return (
     <div>
-
-    </div>
+      <Header />
+        <main>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/viewgames" element={<ViewGames />} />
+          <Route path="/viewgames/:id" element={<GameDetails />} />
+      </Routes>
+    </main>
+  </div>
+  
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Search from '../components/Search'
+import GameCard from '../components/GameCard'
+import GenreCard from '../components/GenreCard'
+import axios from 'axios'
+import { GENRE_URL } from '../globals'
+import { SEARCH_PATH } from '../globals'
+const API_KEY = import.meta.env.VITE_RAWG_KEY
 
 const Home = () => {
   const [genres, setGenres] = useState([])
@@ -6,34 +13,66 @@ const Home = () => {
   const [searched, toggleSearched] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
 
-  const getGenres = async () => {
-
-  }
-
-  const getSearchResults = async (e) => {
-    e.preventDefault()
-  }
-
   const handleChange = (event) => {
-
+    setSearchQuery(event.target.value)
   }
+  
+  useEffect (() => {
+    const getGenres = async () => {
+      const response = await axios.get(`${GENRE_URL}?key=${API_KEY}`)
+      setGenres(response.data.results)
+    }
+    getGenres()
+  },[])
+  
+
+    const getSearchResults = async (e) => {
+      e.preventDefault()
+      const response = await axios.get(`${SEARCH_PATH}?key=${API_KEY}&search=${searchQuery}`)
+      setSearchResults(response.data.results)
+      toggleSearched(!searched)
+    }
+    getSearchResults()
+
 
   return (
     <div>
       <div className="search">
         <h2>Search Results</h2>
-        <section className="search-results container-grid">
-
-        </section>
+        <Search
+          value={searchQuery}
+          onChange={handleChange}
+          onSubmit={getSearchResults}
+        />
+        {searched && (
+          <section className="search-results container-grid">
+            {searchResults.map((result) => (
+              <GameCard
+                key={result.id}
+                image={result.background_image}
+                name={result.name}
+                rating={result.rating}
+              />
+            ))}
+          </section>
+        )}
       </div>
+
       <div className="genres">
         <h2>Genres</h2>
         <section className="container-grid">
-
+          {genres.map((genre) => (
+            <GenreCard
+              key={genre.id}
+              image={genre.image_background}
+              name={genre.name}
+              gamesCount={genre.games_count}
+            />
+          ))}
         </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home
