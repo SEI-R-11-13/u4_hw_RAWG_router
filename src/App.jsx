bb55d98af0c3b237b00309c821fd3030b9a8fa09
HarmonyMorse <harmonythedev@gmail.com>
2024-01-31 14:33:16 -0800

    Add Home with Search

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './styles/App.css'
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import About from './pages/About'
+import Home from './pages/Home'
 
 const App = () => {
 
@@ -11,7 +12,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/about" element={<About />} />
-          {/* <Route path="" element={} /> */}
+          <Route path="/" element={<Home />} />
           {/* <Route path="" element={} /> */}
           {/* <Route path="" element={} /> */}
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Search from '../components/Search'
+import GameCard from '../components/GameCard'
+import GenreCard from '../components/GenreCard'
+import axios from 'axios'
 
 const Home = () => {
   const [genres, setGenres] = useState([])
@@ -7,29 +11,74 @@ const Home = () => {
   const [searchQuery, setSearchQuery] = useState('')
 
   const getGenres = async () => {
-
+    try {
+      const res = await axios.get(`https://api.rawg.io/api/genres?key=${import.meta.env.VITE_RAWG_KEY}`)
+      console.log(res)
+      setGenres(res.data.results)
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   const getSearchResults = async (e) => {
     e.preventDefault()
+    try {
+      const res = await axios.get(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_RAWG_KEY}&search=${searchQuery}`)
+      console.log(res)
+      setSearchResults(res.data.results)
+      toggleSearched(true)
+      setSearchQuery('')
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   const handleChange = (event) => {
-
+    setSearchQuery(event.target.value)
   }
+
+  useEffect(() => {
+    getGenres()
+  }, [])
 
   return (
     <div>
-      <div className="search">
-        <h2>Search Results</h2>
-        <section className="search-results container-grid">
-
-        </section>
-      </div>
+      <form onSubmit={getSearchResults}>
+        <input type="text" placeholder='Search games' onChange={handleChange} />
+        <button type="submit">Search</button>
+      </form>
+      {searched && searchResults.length !== 0 ? (
+        <div className="search">
+          <h2>Search Results</h2>
+          <section className="search-results container-grid">
+            {searchResults.map((game) => (
+              <GameCard
+                key={game.id}
+                onClick=""
+                image={game.background_image}
+                name={game.name}
+                rating={game.rating}
+              />
+            ))}
+          </section>
+        </div>
+      ) : (
+        searched ? (
+          <p>No results found</p>
+        ) : null
+      )}
       <div className="genres">
         <h2>Genres</h2>
         <section className="container-grid">
-
+          {genres.map((genre) => (
+            <GenreCard
+              key={genre.id}
+              image={genre.image_background}
+              name={genre.name}
+              gamesCount={genre.games_count}
+              onClick=""
+            />
+          ))}
         </section>
       </div>
     </div>
